fa29ae3bcdc8ca683368fe15f589639f59830a75
Seth Moore <sethmoore@protonmail.com>
2021-02-23 13:25:14 -0500

    Update entrypoint and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,31 +4,54 @@ FROM steamcmd/steamcmd:ubuntu
 ENV APP_ID "896660"
 # Later, possibly support 'unstable' and 'beta' branches
 ENV APP_BRANCH "public"
-# Container install directory for dedicated server files
-ENV INSTALL_DIR "/opt/valheim_server"
-# world and configuration files
-ENV DATA_DIR "/root/.config/unity3d/IronGate/Valheim"
 
+ENV APP_HOME /valheim
+
+# the application uses only one port as of now, 2457 on UDP by default
+ENV APP_PORT "2457"
+
+# Container install directory for dedicated server files
+ENV INSTALL_DIR "$APP_HOME/valheim_server"
+# world and configuration files
+ENV DATA_DIR "$APP_HOME/.config/unity3d/IronGate/Valheim"
+
+ARG DEBIAN_FRONTEND=noninteractive
 # The helper scripts require these packages
 RUN \
     apt update && \
-    apt install -y \
+    apt install -y --no-install-recommends \
         python3 \
         python3-requests \
         python3-pip && \
     pip3 install python-a2s
 
+### XXX: remove
+# Create the application user
+# RUN useradd -m -d $APP_HOME $APP_USER
+
+# Switch user and set working dir
+# USER $APP_USER
+WORKDIR $APP_HOME
+
+# docker is weird with these variables
+# ENV USER $APP_USER
+ENV HOME $APP_HOME
+
+# COPY --chown=$APP_USER . $APP_HOME/
+COPY . $APP_HOME/
+# RUN chown -R $APP_USER $APP_HOME
+
 # update steamcmd
-RUN \
-    steamcmd +quit
+RUN steamcmd +quit
 
-ADD . .
 
-EXPOSE 2456-2458
-EXPOSE 2456-2458/udp
+# Server does not appear to use TCP
+# EXPOSE 2456-2458
+# EXPOSE 2456-2458/udp
+EXPOSE $APP_PORT/udp
 
-VOLUME [$DATA_DIR, $INSTALL_DIR]
+VOLUME ["$DATA_DIR", "$INSTALL_DIR"]
 
 # unset steamcmd parent entrypoint
-ENTRYPOINT []
-CMD ["entrypoint.sh"]
+ENTRYPOINT [""]
+CMD ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,33 @@
 
 UNKNOWN_VERSION='0000000'
 ERROR_VERSION='ERROR'
-VERSION_FILE='current_version.txt'
-LATEST_VERSION_FILE='latest_version.txt'
+
+
+init() {
+  if [ -z "$INSTALL_DIR" ]; then
+    echo 'WARNING: INSTALL_DIR is unset!' 1>&2
+    echo '         Assuming you want /opt/valheim_server' 1>&2
+    INSTALL_DIR="/opt/valheim_server"
+  fi
+  VERSION_FILE="${INSTALL_DIR}/current_version.txt"
+  LATEST_VERSION_FILE="${INSTALL_DIR}/latest_version.txt"
+}
 
 update_server() {
   if ! ./scripts/update_valheim.sh; then
     echo "ERROR: updating valheim failed!" 1>&2
     exit 2
   fi
+
+  # update the version file after we have successfully updated
+  # (and not forcefully exited)
+  mv "$LATEST_VERSION_FILE" "$VERSION_FILE"
 }
 
 compare_versions() {
   # create a dummy current_version.txt if it doesn't exist
   if ! [ -e "$VERSION_FILE" ]; then
-    echo "$UNKNOWN_VERSION" > "${INSTALL_DIR}/${VERSION_FILE}"
+    echo "$UNKNOWN_VERSION" > "$VERSION_FILE"
   fi
 
   read current_version < "$VERSION_FILE"
@@ -27,6 +40,7 @@ compare_versions() {
 
   # write the latest version to a file
   echo "$latest_version" > "$LATEST_VERSION_FILE"
+  echo "$current_version" "$latest_version" 1>&2
 
   if [ "$current_version" != "$latest_version" ]; then
     printf 'yes'
@@ -37,14 +51,14 @@ compare_versions() {
 
 
 main() {
-  if [ -z "$INSTALL_DIR" ]; then
-    echo 'WARNING: INSTALL_DIR is unset!' 1>&2
-    echo '         Assuming you want /opt/valheim_server' 1>&2
-    INSTALL_DIR="/opt/valheim_server"
-  fi
+  init
 
-  if ! [ -e "$INSTALL_DIR"]; then
+  if ! [ -e "$INSTALL_DIR" ]; then
     echo 'INFO: $INSTALL_DIR does not exist. Installing...'
+    if ! mkdir -p "$INSTALL_DIR"; then
+      echo "ERROR: could not create INSTALL_DIR: $INSTALL_DIR"
+      exit 2
+    fi
     do_update="yes"
   fi
 
@@ -63,10 +77,6 @@ main() {
   elif [ "$do_update" = "no" ]; then
     echo "INFO: Server already up to date. Nothing to do."
   fi
-
-  # update the version file after we have successfully updated
-  # (and not forcefully exited)
-  mv "$LATEST_VERSION_FILE" "$VERSION_FILE"
 }
 
 main "$@"
